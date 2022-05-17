53af5761072e9fe81fa6ef0a19b30fdbec1b5fa3
nlninosi <nlninosi@gmail.com>
2022-05-17 18:56:11 -0500

    m

--- register/src/components/login/login.jsx
+++ register/src/components/login/login.jsx
@@ -3,26 +3,37 @@ import whiteLogo from "../../whiteLogo.svg";
 export class Login extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            email : "",
+            password : ""
+        };
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        //this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleEmailChange(event) {
+        this.setState({email: event.target.value});
+    }
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
     }
     render(){
-        return (
+        return ( 
         <div className="ContenedorBase" ref={this.props.containerRef}>
             <div className="header">
                 <div className="content">
                     <div className="logo">
 
                     </div>
-                    <div className="info">
+                    <form className="info">
                         <div className="form">
                             <label htmlFor="email">Email</label>
-                            <input type="text" name="email" placeholder="email"/>
-                        </div>
-                        <div className="form">
+                            <input type="text" placeholder="email" value={this.state.email} onChange={this.handleEmailChange}/>
                             <label htmlFor="password">Contraseña</label>
-                            <input type="text" name="password" placeholder="password"/>
+                            <input type="text" name="password" placeholder="password" value={this.state.password} onChange={this.handlePasswordChange}/>
                         </div>
-                    </div>
-                    <div className="info">
+                    </form>
+                    <div className="sub">
                         <button type="button" className="submit">Iniciar Sesion</button>
                     </div>
                 </div>
--- register/src/components/login/register.jsx
+++ register/src/components/login/register.jsx
@@ -2,6 +2,23 @@ import React from "react";
 export class Register extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            email : "",
+            password : "",
+            username: ""
+        };
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleUserNameChange = this.handleUserNameChange.bind(this);
+    }
+    handleEmailChange(event) {
+        this.setState({email: event.target.value});
+    }
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
+    }
+    handleUserNameChange(event) {
+        this.setState({usermame: event.target.value});
     }
     render(){
         return( <div className="ContenedorBase" ref={this.props.containerRef}>
@@ -13,15 +30,15 @@ export class Register extends React.Component{
                     <div className="info">
                         <div className="form">
                             <label htmlFor="email">Email</label>
-                            <input type="text" name="email" placeholder="email"/>
+                            <input type="text" name="email" placeholder="email" value={this.state.email} onChange={this.handleEmailChange}/>
                         </div>
                         <div className="form">
                             <label htmlFor="username">Nombre de usuario</label>
-                            <input type="text" name="username" placeholder="username"/>
+                            <input type="text" name="username" placeholder="username" value={this.state.username} onChange={this.handleUserNameChange}/>
                         </div>
                         <div className="form">
                             <label htmlFor="password">Contraseña</label>
-                            <input type="text" name="password" placeholder="password"/>
+                            <input type="text" name="password" placeholder="password" value={this.state.password} onChange={this.handlePasswordChange}/>
                         </div>
                     </div>
                     <div className="info">
